bf113148d1c2569ba74491d5cb99ccec880afba6
jidaqqa <jihadmdaqqa@gmail.com>
2021-12-26 06:11:35 -0800

    Correcting Authentication without encryption!

--- util/authentication_plugin.py
+++ util/authentication_plugin.py
@@ -6,22 +6,23 @@ class Authentication(object):
     @staticmethod
     def read_password_file(password_file, username, password):
         authenticated = False
+        users = dict()
         try:
             with open(password_file) as f:
                 logger.logging.debug(f"\tReading user database from %s" % password_file)
                 for l in f:
                     line = l.strip()
                     if not line.startswith('#'):  # Allow comments in files
-                        (usr, pwd) = line.split(sep=":", maxsplit=3)
-                        logger.logging.debug(f"\tUsername is %s" % usr)
-                        logger.logging.debug(f"\tUsername is %s" % pwd)
-                        if usr == username:
-                            if pwd == password:
-                                authenticated = True
-                            else:
-                                logger.logging.debug("Password is not correct, please check it!")
-                        else:
-                            logger.logging.debug("username is not correct, please check it!")
+                        usr, pwd = line.split(sep=":", maxsplit=3)
+                        users[usr] = pwd
+
+                if username in users.keys():
+                    if users[username] == password:
+                        authenticated = True
+                    else:
+                        logger.logging.debug("Password is not correct, please check it!")
+                else:
+                    logger.logging.debug("username is not correct, please check it!")
         except FileNotFoundError:
             logger.logging.debug("Password file %s not found" % password_file)
         return authenticated
